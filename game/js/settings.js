bd4a5964a29492c3caa05524002324749c1dd54a
RyaraSUKI <ryara9962@gmail.com>
2025-03-19 22:48:44 +0800

    Last Sync: 2025-03-19 14:48:44 (Mobile)

--- game/js/settings.js
+++ game/js/settings.js
@@ -1,102 +1,59 @@
 /* 感谢manonamora的设置模板代码!Thanks for manonamora's Settings Template source code! */
-
-//~ ~ ~ ~ SETTINGS OPTIONS ~ ~ ~ ~
-    /*
-     Notes:
-        ~ Settings are displayed in the order it was coded in this section. If you do not like the order below, just move the code around.
-        ~ Settings should fit what your project needs, so add what is necessary and remove what is unwanted.
-            ~ Same with the Headers and descriptions of headers/settings. Just because I wrote it this way doesn't mean you have to.
-        ~ The code below include the Settings most used or requested in Interactive-Fiction communities, when it comes to accessibility.
-    */
-
-    //~ ~ ~ ~ TEXT DISPLAY ~ ~ ~ ~
-    Setting.addHeader("Text Display", "Below are settings controlling how the text is displayed");
-    /*
-     Note:
-        ~ Headers are coded as follow: 
-            Setting.addHeader(name [, description]);
-          with the description being optional. Other headers coded here are without descriptions.
-    */
-        
-        //~ ~ Font Type ~ ~
-    /*
-        This is the most used and recommended Setting when creating a project. Since there isn't a font that everyone find comfortable to read, adding multiple options makes your project accessible to more people.
-        The Setting below only account for 3 different options: a serif, sans-serif, and monospace font. It is recommended to include multiple (~2) fonts for each type, so readers have choices when playing.
-            > you can name the classes based on the fonts you're importing, i'm just using the types here as an example.
-    */
-var settingFontFamily = ["Serif", "Sans Serif", "Monospace"]; 
-    //The selectable options, name visible to the player
-    //Add or remove them (each should be separated by a comma)
+// 主题切换
+Setting.addHeader("主题");
+var settingThemeNames = ["明弦音（亮色主题）", "夜隠染（暗色主题）"]; 
+var changeTheme = function () {
+    var $html = $("html");
+    $html.removeClass("rev");
+    $("#passages").removeClass("theme-d");
+    $("#passages").addClass("theme-l");
+    switch (settings.theme) {
+        case "夜隠染（暗色主题）":
+            $html.addClass("rev");
+            $("#passages").removeClass("theme-l");
+            $("#passages").addClass("theme-d");
+            break;
+    }
+};
+Setting.addList("theme", {
+    label       : "主题切换",
+    desc        :"在这里选择你喜欢的主题吧！",
+    list        : settingThemeNames,
+    default	    : "夜隠染（暗色主题）",
+    onInit      : changeTheme,
+    onChange    : changeTheme
+});
+// 文本显示
+    Setting.addHeader("文本显示", "在这里调整页面正文的字体、文字大小、行高与对齐哟~");
+//字体切换
+var settingFontFamily = ["系统字体", "像素字体"]; 
 var fontFamily = function() {
     var $html = $("html"); 
-    $html.removeClass("serif sansserif monospace"); 
-        //this code above will remove any font class used before the switch
+    $html.removeClass("default unifont");
     switch (settings.fontFamily) { 
-        //this section will add the relevant font class, depending on the player's choice
-        case "Serif": 
-            // the case name should be the same as the option defined in the first line
-            $html.addClass("serif"); 
-                //each option need to be set in the StyleSheet (CSS), through a class 
-                //[.class]
+        case "系统字体": 
+            $html.addClass("default"); 
             break;
-        case "Sans Serif":
-            $html.addClass("sansserif");
+        case "像素字体":
+            $html.addClass("unifont");
             break;
-        case "Monospace":
-            $html.addClass("monospace");
-            break;
-        /*
-        To add a new font to the list:
-            ~ Add the Name of the new option inside > var settingFontFamily
-            ~ Add a CSS font class inside > $html.removeClass
-            ~ Add another case block inside > switch (settings.fontFamily)
-
-            case "Font Option"
-                $html.addClass("css-tag-for-the-new-font");
-                break;
-        */
     }
 };
 Setting.addList("fontFamily", {
-    //This is the actual Setting, visible to the player
-    label		: "Change font type",
-    desc        : "Choose between a Serif, Sans-serif, or the OpenDyslexic Font", 
-        //descriptions of settings are optional, so you can edit or remove this line
+    label		: "切换字体",
+    desc        : "选择你想要的字体吧！", 
     list		: settingFontFamily,
-    default     : "Serif", 
-        //If you delete the default line, the system will pick the first selectable name option as the new default
+    default     : "系统字体", 
     onInit		: fontFamily,
     onChange	: fontFamily
-});	
-
-    //~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
-
-        //~ ~ Font Size ~ ~
-    /*
-        There are *many* ways to include a Font Size Settings, from using a size slider instead of a drop down, to using class defined in the StyleSheet (logic similar to the previous settings). The defined option below uses a Dropdown option, with pure JavaScript to assign a new font size to #passages when that Setting is interacted with.
-    */
+});
+// 文字大小
 var settingFontSize = ["75%", "100%", "125%", "150%"];
-    //The selectable options, name visible to the player
-    //Add or remove them (each should be separated by a comma)
 var resizeFont = function() {
     var size = document.getElementById("passages"); 
-        //This is linked to #passages in the CSS
-    /*
-        From here, you have 2 options:
-        - assigning automatically the selectable option (%) to the element
-        - defining manually each size in other units (px, em, large/small...)
-
-        Since the Setting can only handle one at a time, Option 1 is "hidden" as a comment.
-    */
-    // > Option 1:
-    // size.style.fontSize = settings.fontSize;
-
-    // > Option 2:
     switch (settings.fontSize) {
         case "75%":
             size.style.fontSize = "75%";
-            // Instead of "75%", you could have "small" or "12px" or "0.75em", etc...
-            // But if you're sticking to %, just use Option 1 instead.
             break;
         case "100%":
             size.style.fontSize = "100%";
@@ -110,35 +67,22 @@ var resizeFont = function() {
     }
 };
 Setting.addList("fontSize", {
-    label		: "Change Font Size",
+    label		: "调整文字大小",
+    desc        : "在这里调整字体的显示大小吧！",
     list		: settingFontSize,
     default     : "100%",
     onInit		: resizeFont,
     onChange	: resizeFont
 });
-
-    //~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
-
-        //~ ~ Line Height ~ ~
-    /*
-        This is more of an uncommon setting, as line-heights (space between each line) are not usually modified.
-        The code below is essentially the same as the Font Type setting, but targetting a different CSS rule in the StyleSheet.
-    */
+// 切换行高
 var settingLineHeight = ["75%", "100%", "125%", "150%"];
-    //The selectable options, name visible to the player
-    //Add or remove them (each should be separated by a comma)
-    //You can also edit it, like "75%" becomes "Small"
 var lineHeight = function () {
     var $html = $("html");
     $html.removeClass("lh-small lh-medium lh-large lh-biggest");
-        //this code above will remove any font class used before the switch
     switch (settings.lineheight) {
-        //this section will add the relevant font class, depending on the player's choice
+
         case "75%":
-            // the case name should be the same as the option defined in the first line
             $html.addClass("lh-small");
-                //each option need to be set in the StyleSheet (CSS), through a class 
-                //[.class]
             break;
         case "100%":
             $html.addClass("lh-medium");
@@ -152,244 +96,68 @@ var lineHeight = function () {
     }
 };
 Setting.addList("lineheight", {
-    label       : "Change Line Height",
+    label       : "调整行高",
+    desc        : "在这里调整合适的行高距离吧！",
     default     : "100%",
     list        : settingLineHeight,
     onInit      : lineHeight,
     onChange    : lineHeight
 });
-    
-    //~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
-
-        //~ ~ Text Alignment ~ ~
-    /*
-        Unlike the previous Settings, which are Drop-down lists, this one is a Toggle, which will change the alignment of the text from justified to left (and vice-versa).
-        If you are planning on including other alignments (right, center), you should change the setting into a Drop-down instead. This version can only take into account 2 options.
-    */
+// 文本对齐
 var settingtextAlign = function () {
-    if (settings.textalign) { // setting is true
+    if (settings.textalign) { 
         $("html").addClass("justified");
-            // if enabled, the setting with add the class [.justified] to the page
-            // the class defined in the StyleSheet will justify the text.
-    } else { // setting is false
+    } else {
         $("html").removeClass("justified");
 }};
 Setting.addToggle("textalign", {
-    label       : "Change Text Alignment",
-    desc        : "If enabled, the text will be justified",
+    label       : "切换文本对齐",
+    desc        : "在这里开启文本绝对位置对齐吧！",
     default     : false,
     onInit      : settingtextAlign,
     onChange    : settingtextAlign
 });
-
-//~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ 
-
-    //~ ~ ~ ~ SOUND/THEME ~ ~ ~ ~
-Setting.addHeader("Mode and Volume");
-
-        //~ ~ Theme ~ ~
-    /*
-        Another very popular setting, especially since Light/Dark modes are divisive (some peeps prefer light palettes, other dark ones). While you could use [@media (prefers-color-scheme: dark/light)] to set themes depending on the user's browser preferences, it is recommended to let the player choose which ever theme they prefer in the moment (Light, Dark, or other).
-        The Setting essentially allows the use of different colour palette, which can be harnessed to give different vibes to the story/game.
-        
-            Either way, you should ensure the colours chosen in your palette are accessible (i.e. have enough contrast to be readable and comfortable). 
-    
-        The example here displays a simple Light/Dark option.
-    */
-var settingThemeNames = ["Light", "Dark"]; 
-    //The selectable options, name visible to the player
-    //Add more options if wanted (each should be separated by a comma)
-    //If kept at 2, you could change it to a Toggle instead.
-var changeTheme = function () {
-    var $html = $("html");
-    $html.removeClass("rev");
-    $("#passages").removeClass("theme-d");
-    $("#passages").addClass("theme-l");
-    switch (settings.theme) {
-        case "Dark":
-            $html.addClass("rev");
-            $("#passages").removeClass("theme-l");
-            $("#passages").addClass("theme-d");
-            break;
-        /*
-         To add a new theme to the list:
-            ~ Add the Name of the new Theme inside > var settingThemeNames
-            ~ Add a CSS font class inside > $html.removeClass
-                $html.removeClass("rev new-class")
-            ~ Add another case block inside > switch (settings.theme)
-
-            case "Theme-XYZ"
-                $html.addClass("the-new-theme-class");
-                break;
-        */
-    }
-};
-Setting.addList("theme", {
-    label       : "Switch display mode",
-    list        : settingThemeNames,
-    default	    : "Dark",
-    onInit      : changeTheme,
-    onChange    : changeTheme
-});
-
-    //~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
-
-        //~ ~ Volume ~ ~
-    /*
-        This Setting is mainly useful if you plan on adding sound (music/SFX) to your game. It will only control the general volume (Master Volume) of the sound played, not the specific track being played (this is done with the <<audio>> macro).
-
-        The Setting will be useless for Mobile users, as mobile browser override the volume with the device's volume. In computer browsers, players can change the volume of both their device's and the tab the game is run on, without touching this setting.
-
-        By fiddling with the [function ()] and the Audio APIs (pretty advanced), you could create volume sliders for groups of tracks (background/voice over/ etc...).
-    */
-var settingMasterVolumeHandler = function () {
-	SimpleAudio.volume(settings.masterVolume / 10);
-};
-Setting.addRange("masterVolume", {
-	label    : "Master volume.",
-	min      : 0,
-	max      : 10,
-	step     : 1,
-	onInit   : settingMasterVolumeHandler,
-	onChange : settingMasterVolumeHandler
-});
-
-//~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ 
-
-
-    //~ ~ ~ ~ OTHER ACCESSIBILITY ~ ~ ~ ~
-Setting.addHeader("Accessibility");
-
-        //~ ~ Animation Toggle ~ ~
-    /*
-        If you are going to include *any* form of animation (changes in movement/colour/display or including .gif), you should include a setting to hide those, so users with sensitivity to animated content (e.g. epilepsy, photosensitivity, motion sickness...).
-
-        The Toggle below is can be used for both elements defined in the StyleSheet, and in passages themselves (see the Demo page).
-    */
-var settingAnimation = function () {
-    if (settings.textanim) { // setting is true
-        $("html").removeClass("nogif");
-            // if enabled, the setting with remove the class [.nogif] from the page
-            // this will display any animated content defined in the Stylesheet
-    } else { // setting is false
-        $("html").addClass("nogif");
-}};
-Setting.addToggle("textanim", {
-    label       : "Show Animation",
-    desc        : "If disabled, there won't be any animated element on the page.",
-    default     : true,
-    onInit      : settingAnimation,
-    onChange    : settingAnimation
-});
-
-    //~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
-
-        //~ ~ Notification Toggle ~ ~
-    /*
-        This Setting is another form of the Toggle-type, in that it *doesn't* link the setting to a CSS class. You only get a setting variable that can be used in-game to display/hide elements (like a notification popup).
-        
-        You can still change it to the other version, to hide your notifications/popups through the Stylesheet instead.
-
-        ~ If your notification is animated, you could also simply combine the Animation and Notification Toggles into one single setting.
-    */
-Setting.addToggle("notif", { 
-    label       : "Show Notification",
-    desc        : "If disabled, you will not be notified.",
-    default     : true,
-});
-
-
-//~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ 
-
-    //~ ~ ~ ~ SAVING (Auto/Name) ~ ~ ~ ~
-Setting.addHeader("Save Settings");
-
-    /* 
-     The Config below will check:
-        - the type of save (auto or browser slot)
-            - for autosave: if the autosave setting  is enabled
-        - then whether a passage is tagged with [noreturn]
-     If the passage isn't tagged, it will save the game (auto/manual), otherwise, nothing will happen.
-
-     If you want to have more tags restricting saving:
-        - for any passage including either tags
-            tags().includesAny('noreturn', 'another-tag', 'something-else')
-        - for any passage including ALL the tags:
-            tags().includesAll('noreturn', 'another-tag', 'something-else')
-    */
+// 存档
+Setting.addHeader("迷子的日记本设定");
 Config.saves.isAllowed = function (saveType) {
     if (saveType === Save.Type.Auto) {
-        // this section is just for the autosave
-        if (settings.autosave && !tags().includes('noreturn')){
-            // this checks that the player has enabled autosaves + isn't on a tagged passage
-            return true;
-        }
-        return false;
-    }
-    else {
-        if (tags().includes('noreturn')) {
-            //this checks that the player isn't on a tagged passage
+        if (settings.autosave && !tags().includes('autosave')){ //检查passage是否带有autosave标签，若有则自动存档
             return false;
         }
         return true;
     }
+    else {
+        if (tags().includes('autosave')) {
+            return true;
+        }
+        return true;
+    }
 };
-
-    //~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
-
-        //~ ~ Autosave Toggle ~ ~
-    /*
-        To include an Autosave Setting in your project, you need to enable at least one slot (the Config line below) and ensure the playthrough isn't saved if the player disabled the setting (Config block above).
-        Then, you just need a simple toggle. 
-    */
 Config.saves.maxAutoSaves = 1;
 Setting.addToggle("autosave", {
-    label       : "Autosaves",
-    default     : false,
+    label       : "自动记录",
+    desc        :"在这里开启/关闭自动记录功能吧！",
+    default     : true,
 });
-
-    //~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
-
-        //~ ~ Autoname Toggle ~ ~
-    /* 
-        Before creating the Toggle, we need to define *how* the name of a save will look like - as well, as making a clear distinction between Autosaves and Manual browser saves. 
-            > By default, SugarCube uses the number of "turns" (seen passages) the player has done.
-
-        First, we'll have to check the save type (auto/manual), then if autoname is enabled. The Autosave will always be automatically named (otherwise the player will get a prompt on every passage to enter a new name).
-
-        The Save Name defined below will be simple: using the name of the PC + current chapter -> both saved in variables.
-            > If you're not using these variables in your project, just change it to whatever fits best!
-            > State.getVar("$var") = State.variables.var
-
-        If you plan on using the name of a passage, you will need to include [passage()].
-            > Note: you should be sure the name of passages are meaningful, like "Entrance to Manor" instead of "Ch10-2"
-        
-    */
-
 Config.saves.descriptions = function (saveType) {
     switch (saveType) {
-      //This checks what type of save we're dealing with (auto or manual)
         case Save.Type.Auto: {
-            return "Autosave: " + (State.getVar("$FirstName") ? State.getVar("$FirstName") : '???') + " - " + State.getVar("$chapter");
-            // the second section (after +) will check if $FirstName is defined, before either printing the value (defined) or ??? (not defined)
-            // An alternative:
-            // return "Autosave: " + passage();
+            return "自动记录: " + passage();
             break;
         }
         default: {
             if (settings.autoname) {
-                return (State.getVar("$FirstName") ? State.getVar("$FirstName") : '???') + " - " + State.getVar("$chapter");
+                return "迷子的日记" + " - " + passage();
             }
             else {
-                return prompt("Enter Save Name:", passage());
-                // I've included [passage()] here as the text inside the prompt box, but you can change it to whatever, like "" (empty) or "SAVE!" or State.getVar("$var")...
+                return prompt("请写下记录名：", "迷子的日记" + " - " + passage());
             }
         }
     }
 };
 
 Setting.addToggle("autoname", {
-    label       : "Autoname",
-    desc        : "Title of Save Slots if enabled.",
+    label       : "自动命名",
+    desc        : "在这里开启/关闭记录自动命名吧！（对应写下记录的页面名）",
     default     : false,
 });
---
+++ game/js/zh-CN.js
@@ -0,0 +1,244 @@
+/***********************************************************************************************************************
+
+	locale/zh-CN.js – 简体中文
+
+	Localization by: Alt236679.
+	Edited by: RyaraSUKI , For MyGoEveryday !!!!!
+
+	Copyright © 2017–2025 Thomas Michael Edwards <thomasmedwards@gmail.com>. All rights reserved.
+	Use of this source code is governed by a BSD 2-clause "Simplified" License, which may be found in the LICENSE file.
+
+	For more information about the guidelines used to create this localization, see:
+		http://www.motoslave.net/sugarcube/2/docs/#guide-localization
+
+***********************************************************************************************************************/
+/* global l10nStrings */
+/* eslint-disable strict */
+
+(function () {
+	/*******************************************************************************
+		General.
+	*******************************************************************************/
+
+	l10nStrings.textAbort = '中止';
+
+	l10nStrings.textAborting = '操作中止';
+
+	l10nStrings.textCancel = '取消';
+
+	l10nStrings.textClear = '清空';
+
+	l10nStrings.textClose = '关闭';
+
+	l10nStrings.textDelete = '删除';
+
+	l10nStrings.textExport = '导出';
+
+	// In lowercase, if possible.
+	l10nStrings.textIdentity = '游戏';
+
+	l10nStrings.textImport = '导入';
+
+	l10nStrings.textLoad = '加载';
+
+	l10nStrings.textOff = '关闭';
+
+	l10nStrings.textOk = '确定';
+
+	l10nStrings.textOn = '开启';
+
+	l10nStrings.textSave = '记录';
+
+	// (noun) chance to act (in a game), moment, period
+	l10nStrings.textTurn = '迷子的记忆';
+
+
+	/*******************************************************************************
+		Errors.
+	*******************************************************************************/
+
+	// NOTE: `passage` is supplied locally.
+	l10nStrings.errorNonexistentPassage = '页面"{passage}"不存在';
+
+
+	/*******************************************************************************
+		Warnings.
+	*******************************************************************************/
+
+	l10nStrings.warningNoStorage = '提供存储功能的API缺失。可能是由于第三方Cookie设置被禁用（这也会影响Web Storage API），或处于隐私浏览模式。';
+
+	l10nStrings.warningNoWebStorage = 'Web Storage API缺失，因此该{textIdentity}在降级模式下运行。你可以继续，但一些部分可能无法正常工作。';
+
+	l10nStrings.warningDegraded = '某些支持该{textIdentity}所需的功能缺失，因此运行于降级模式下。你可以继续，但一些部分可能无法正常工作。';
+
+	l10nStrings.warningNoSaves = '某些支持保存所需的功能缺失，因此在本次会话中已禁用存档。';
+
+
+	/*******************************************************************************
+		API: Save.
+	*******************************************************************************/
+
+	l10nStrings.saveErrorDisallowed = '现在还不能留下迷子的足迹……';
+
+	l10nStrings.saveErrorDecodeFail = '无法读取迷子的记录，该记录可能已经损坏';
+
+	l10nStrings.saveErrorDiskLoadFail = '读取迷子的记录失败';
+
+	l10nStrings.saveErrorIdMismatch = '这不是迷子的记忆！';
+
+	l10nStrings.saveErrorInvalidData = '缺少迷子的数据，该记录可能已经损坏';
+
+	l10nStrings.saveErrorNonexistent = '记录迷子了';
+
+
+	/*******************************************************************************
+		Base UI.
+	*******************************************************************************/
+
+	l10nStrings.uiBarLabelToggle = '展开/收起UI栏';
+
+	l10nStrings.uiBarLabelBackward = '在{textIdentity}迷子日记中后退';
+
+	l10nStrings.uiBarLabelForward = '在{textIdentity}迷子日记中前进';
+
+	// [DEPRECATED]
+	l10nStrings.uiBarLabelJumpto = '回忆至{textIdentity}迷子日记中的某一点';
+
+
+	/*******************************************************************************
+		Dialog: Alert.
+	*******************************************************************************/
+
+	l10nStrings.alertTitle = '警告';
+
+
+	/*******************************************************************************
+		Dialog: Restart.
+	*******************************************************************************/
+
+	l10nStrings.restartTitle = '重新迷子';
+
+	l10nStrings.restartMesgPrompt = '即将重启，所有未记录的都将迷失——确定要重新载入吗？';
+
+
+	/*******************************************************************************
+		Dialog: Saves.
+	*******************************************************************************/
+
+	l10nStrings.continueTitle = '继续';
+
+	l10nStrings.savesTitle = '迷子们的日记本';
+
+	l10nStrings.savesHeaderBrowser = '浏览器内';
+
+	l10nStrings.savesHeaderDisk = '本地';
+
+	l10nStrings.savesLabelBrowserClear = '清空浏览器里迷子的记录';
+
+	l10nStrings.savesLabelBrowserExport = '导出浏览器记录为bundle';
+
+	l10nStrings.savesLabelBrowserImport = '从bundle导入浏览器记录';
+
+	l10nStrings.savesLabelDiskLoad = '从本地加载';
+
+	l10nStrings.savesLabelDiskSave = '保存至本地';
+
+	l10nStrings.savesTextBrowserAuto = '自动记录';
+
+	l10nStrings.savesTextBrowserSlot = '记录槽';
+
+	l10nStrings.savesTextNoDate = '未知时间';
+
+
+	/*******************************************************************************
+		Dialog: Settings.
+	*******************************************************************************/
+
+	l10nStrings.settingsTitle = '迷子们的北极星';
+
+	l10nStrings.settingsTextReset = '重置为默认项';
+
+
+	/*******************************************************************************
+		Debugging: Error Views.
+	*******************************************************************************/
+
+	l10nStrings.errorViewTitle = '错误';
+
+	l10nStrings.errorViewLabelToggle = '展开/收起错误视图';
+
+
+	/*******************************************************************************
+		Debugging: Debug bar.
+	*******************************************************************************/
+
+	l10nStrings.debugBarLabelToggle = '展开/收起调试栏';
+
+	l10nStrings.debugBarLabelViewsToggle = '展开/收起调试视图';
+
+	l10nStrings.debugBarLabelWatchAdd = '添加监视';
+
+	l10nStrings.debugBarLabelWatchAll = '监视所有变量';
+
+	l10nStrings.debugBarLabelWatchClear = '清空所有监视';
+
+	l10nStrings.debugBarLabelWatchDelete = '删除该监视';
+
+	l10nStrings.debugBarLabelWatchPlaceholder = '变量名称';
+
+	l10nStrings.debugBarLabelPassagePlaceholder = '段落名称';
+
+	l10nStrings.debugBarLabelPassagePlay = '播放段落';
+
+	l10nStrings.debugBarLabelWatchToggle = '展开/收起监视面板';
+
+	l10nStrings.debugBarMesgNoWatches = '没有设置监视';
+
+	l10nStrings.debugBarTextAdd = '添加';
+
+	l10nStrings.debugBarTextPassage = '段落';
+
+	l10nStrings.debugBarTextViews = '视图';
+
+	l10nStrings.debugBarTextWatch = '监视';
+
+
+	/*******************************************************************************
+		Macros.
+	*******************************************************************************/
+
+	// (verb) rewind, revert
+	l10nStrings.macroBackText = '回退';
+
+	// (verb) go/send back
+	l10nStrings.macroReturnText = '返回';
+
+
+	/*******************************************************************************
+		[DEPRECATED] Dialog: Autoload.
+	*******************************************************************************/
+
+	l10nStrings.autoloadTitle = '读取自动记录';
+
+	l10nStrings.autoloadMesgPrompt = '检测到自动记录。是加载该记录，还是从头开始？';
+
+	l10nStrings.autoloadTextCancel = '从头开始';
+
+	l10nStrings.autoloadTextOk = '加载记录';
+
+
+	/*******************************************************************************
+		[DEPRECATED] Dialog: Jump To.
+	*******************************************************************************/
+
+	l10nStrings.jumptoTitle = '跳转至';
+
+	l10nStrings.jumptoMesgUnavailable = '当前无可用跳转点\u2026';
+
+
+	/*******************************************************************************
+		[DEPRECATED] Dialog: Share.
+	*******************************************************************************/
+
+	l10nStrings.shareTitle = '分享';
+})();
