e79d9ec99c2fed65b1138579602fe782f8f66028
RyaraSUKI <ryara9962@gmail.com>
2025-05-12 22:59:56 +0800

    5.12 part2

---
+++ game/002_js/progress_data.js
@@ -0,0 +1,25 @@
+if (!localStorage.getItem("playProgress")) {
+	localStorage.setItem("playProgress", JSON.stringify({
+		chapterUnlocked: 1,
+		dailyUnlocked: false,
+		noticeRead: false,
+		unlockedLocations: []
+	}));
+}
+setup.progress = JSON.parse(localStorage.getItem("playProgress"));
+State.variables.unlockedLocations = setup.progress.unlockedLocations.slice();
+
+setup.saveProgress = function (changes) {
+	const current = JSON.parse(localStorage.getItem("playProgress"));
+	const updated = Object.assign({}, current, changes);
+	localStorage.setItem("playProgress", JSON.stringify(updated));
+	setup.progress = updated;
+};
+
+setup.unlockLocation = function (name) {
+	const progress = setup.getProgress();
+	if (!progress.unlockedLocations.includes(name)) {
+		progress.unlockedLocations.push(name);
+		setup.saveProgress({ unlockedLocations: progress.unlockedLocations });
+	}
+};
